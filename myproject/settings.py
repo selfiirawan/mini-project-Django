9dbdf0851689e73f8fd662056d65b91e7b841d55
Selfi <celpi.irawan@gmail.com>
2025-12-08 20:55:09 +0800

    fix SLS issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os 
+import certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
